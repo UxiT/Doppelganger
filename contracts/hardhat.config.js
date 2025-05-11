07086cdd7b38a3894c9c4ac6734ff1fa20d9d6cf
Igor <igormaximov@gmail.com>
2025-05-11 22:50:41 +0300

    Fix verification

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -36,6 +37,9 @@ module.exports = {
             url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
             accounts: [PRIVATE_KEY],
         },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
     },
     paths: {
         sources: "./src",
